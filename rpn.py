f18171caf1206f67675981d6e355aacc4b4e278e
Ruby <rubyj@umich.edu>
2018-11-06 13:05:35 -0500

    initial commit

---
+++ rpn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def calculate(arg):
+    pass
+
+def main():
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
+
+if __name__ == '__main__':
+    main()
+
